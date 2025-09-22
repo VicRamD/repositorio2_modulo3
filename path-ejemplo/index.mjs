2718a8d5d4ab019b9f2f63a8f13cf854bf194459
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2025-09-22 08:39:24 -0300

    añadiendo hasta 'obtener directorio base'

--- path-ejemplo/index.mjs
+++ path-ejemplo/index.mjs
@@ -1,2 +1,10 @@
 import path from 'path';
 
+//Definir una ruta de archivo de ejemplo
+const filePath = './data/example.txt'
+
+//Obtener el directorio base
+const dirName = path.dirname(filePath);
+console.log("Directorio Base: ", dirName);
+
+//
