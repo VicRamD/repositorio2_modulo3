83f3573fd8421cc71a314f43c465dd4476e49b3d
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2025-09-22 14:12:01 -0300

    terminando el primer módulo del TP2

--- path-ejemplo/index.mjs
+++ path-ejemplo/index.mjs
@@ -1,4 +1,8 @@
-import path from 'path';
+import path, { basename } from 'path';
+
+/*En la consola usar cd path-ejemplo
+y luego node index.mjs
+*/
 
 //Definir una ruta de archivo de ejemplo
 const filePath = './data/example.txt'
@@ -7,4 +11,14 @@ const filePath = './data/example.txt'
 const dirName = path.dirname(filePath);
 console.log("Directorio Base: ", dirName);
 
-//
+//obtener nombre del archivo sin extensión
+const baseName = path.basename(filePath, '.txt');
+console.log(`Nombre del archivo: ${baseName}`);
+
+//Obtener la extensión del archivo
+const extName = path.extname(filePath);
+console.log(`Extensión del archivo: ${extName}`);
+
+//Crear una ruta unida
+const newPath = path.join('/user', 'docs', 'newfile.txt');
+console.log(`Nueva ruta: ${newPath}`);
